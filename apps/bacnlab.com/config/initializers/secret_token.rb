5667af95e4d1b319f625d64a4eedb3523139358b
Simon Wex <simon@simonwex.com>
2011-06-21 12:42:18 -0700

    Initial rails project.

---
+++ apps/bacnlab.com/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BacnlabCom::Application.config.secret_token = '77d0a87c5a6cfd70f483cfbbac6a884017e670c07aa2890e63201245022ac74dcae44a3e3d66cbf89d74d86a50efcdbf2e8395c610ef0a630ce6922e85c8e7dc'
