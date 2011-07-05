938139b5b7bc0d64ee1c9f4775971895240bf833
Bryan Clark <clarkbw@gmail.com>
2011-07-05 15:41:45 -0700

    updating the style and renders a bit.

--- apps/sift.mozillalabs.com/public/renderers/com.linkedin/render.js
+++ apps/sift.mozillalabs.com/public/renderers/com.linkedin/render.js
@@ -1,7 +1,19 @@
 //TODO: Remove this and build the plugin architecture
 Dashboard.renderers['com.linkedin'] = {
   render: function(bacn, element){
-    element.find('h1').text(bacn.headers['Subject']);
+    var liclass = bacn.headers['X-LinkedIn-Class'];
+    if (liclass === "INVITE-MBR") {
+      for (var p in bacn.parts) {
+        var name = /Accept invitation from (.*)/.exec(bacn.parts[p]);
+        if (name) {
+          element.find('.name').text(name[1]);
+          element.find('.action').text("wants you to join their network");
+          break;
+        }
+      }
+    } else {
+      element.find('.sender').text(bacn.headers['Subject']);
+    }
     return element;
   }
 }
--- apps/sift.mozillalabs.com/public/renderers/com.twitter.postmaster/render.js
+++ apps/sift.mozillalabs.com/public/renderers/com.twitter.postmaster/render.js
@@ -1,7 +1,29 @@
 //TODO: Remove this and build the plugin architecture
 Dashboard.renderers['com.twitter.postmaster'] = {
   render: function(bacn, element){
-    element.find('h1').text(bacn.headers['Subject']);
+    var senderhandle = bacn.headers['X-Twittersenderscreenname'];
+    var sendername = bacn.headers['X-Twittersendername'];
+    var subject = bacn.headers['Subject'];
+    var created = new Date(Date.parse(bacn.headers['X-Twittercreatedat'])); // Wed Jun 22 19:03:19 +0000 2011
+
+    element.find('.name').text(sendername);
+    element.find('.handle').text(senderhandle);
+
+    if (bacn.headers['X-Twitteremailtype'] === "is_following") {
+      element.find('.action').text("is following you");
+      element.find('.follow').show();
+    } else if (/.*mentioned.*/.test(subject)) {
+      element.find('.action').text("mentioned you");
+      var regex = new RegExp('@'+senderhandle+':(.*)');
+      console.log(regex);
+      for (var p in bacn.parts) {
+        var message = regex.exec(bacn.parts[p]);
+        if (message) {
+          element.find('.message').text(message[1]);
+        }
+      }
+    }
+
     return element;
   }
 }
